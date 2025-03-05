76c6087c4ea2e789e863dc1394821466c56ecdc4
Gyuwon Yi <gyuwon@live.com>
2025-03-06 00:12:56 +0900

    테스트 시나리오 '요청이 성공하면 201 Created 상태코드를 반환한다' 구현

--- src/main/java/tdd/demo/ProductsController.java
+++ src/main/java/tdd/demo/ProductsController.java
@@ -1,10 +1,17 @@
 package tdd.demo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductsController {
 
     public ProductsController() {
+    }
+
+    @PostMapping("/api/products")
+    ResponseEntity<?> createProduct() {
+        return ResponseEntity.created(null).build();
     }
 }
--- src/test/java/tdd/demo/상품_등록_API.java
+++ src/test/java/tdd/demo/상품_등록_API.java
@@ -1,9 +1,32 @@
 package tdd.demo;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class 상품_등록_API {
+
+    @Test
+    void 요청이_성공하면_201_Created_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateProductCommand("키보드", 10000, 100);
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/api/products",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(201);
+    }
 }
